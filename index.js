bcd6e9cde9599eee48fc34a6e95213f04e08b1fd
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-12-06 15:49:49 +0600

    🚧 wip: added express server and a json model

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Welcome to Book Library API...");
+// Dependencies
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+	res.status(200).json({
+		api: "Book Library API",
+		version: "1.0.0",
+		author: "Khalid Saifullah Fuad",
+	});
+});
+
+app.listen(PORT, (err) => {
+	if (err) console.error(err);
+	console.log(`\x1b[33m> Server is running on port ${PORT}...\x1b[0m`);
+});
